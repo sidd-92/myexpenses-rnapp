4f711cbf20bf71e69f76b6bd0066995cba034c2c
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2022-03-31 18:37:51 +0530

    Base Changes

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+	useFonts,
+	Oswald_200ExtraLight,
+	Oswald_300Light,
+	Oswald_400Regular,
+	Oswald_500Medium,
+	Oswald_600SemiBold,
+	Oswald_700Bold,
+} from "@expo-google-fonts/oswald";
 
+import {
+	Raleway_100Thin,
+	Raleway_100Thin_Italic,
+	Raleway_200ExtraLight,
+	Raleway_200ExtraLight_Italic,
+	Raleway_300Light,
+	Raleway_300Light_Italic,
+	Raleway_400Regular,
+	Raleway_400Regular_Italic,
+	Raleway_500Medium,
+	Raleway_500Medium_Italic,
+	Raleway_600SemiBold,
+	Raleway_600SemiBold_Italic,
+	Raleway_700Bold,
+	Raleway_700Bold_Italic,
+	Raleway_800ExtraBold,
+	Raleway_800ExtraBold_Italic,
+	Raleway_900Black,
+	Raleway_900Black_Italic,
+} from "@expo-google-fonts/raleway";
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	let [fontsLoaded] = useFonts({
+		Oswald_200ExtraLight,
+		Oswald_300Light,
+		Oswald_400Regular,
+		Oswald_500Medium,
+		Oswald_600SemiBold,
+		Oswald_700Bold,
+		Raleway_200ExtraLight,
+		Raleway_100Thin,
+		Raleway_500Medium,
+		Raleway_600SemiBold,
+		Raleway_700Bold,
+		Raleway_800ExtraBold,
+		Raleway_900Black,
+	});
+
+	if (!fontsLoaded) {
+		return (
+			<View>
+				<Text>Loading....</Text>
+			</View>
+		);
+	}
+
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					options={{
+						title: "My Home",
+						headerStyle: {
+							backgroundColor: "#f4511e",
+						},
+						headerTintColor: "#fff",
+						headerTitleStyle: {
+							fontFamily: "Raleway_900Black",
+						},
+					}}
+					name="Home"
+					component={HomeScreen}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+
+function HomeScreen() {
+	return (
+		<ScrollView contentContainerStyle={styles.contentContainer}>
+			<Text style={{ fontFamily: "Oswald_500Medium", fontSize: 40 }}>Oswald_500Medium</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Oswald_500Medium", fontSize: 40 }}>Oswald_500Medium</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+			<Text style={{ fontFamily: "Raleway_100Thin", fontSize: 40 }}>Raleway_100Thin</Text>
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	contentContainer: {
+		paddingVertical: 10,
+		marginHorizontal: 10,
+	},
 });
